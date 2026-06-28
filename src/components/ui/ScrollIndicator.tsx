c2a02ccf85df212d6485610b176acc939d94642e
OGLadi <yorubaherdsmehnes@gmail.com>
2026-06-28 17:12:41 +0100

    Fixing the scroll indicator

--- src/components/ui/ScrollIndicator.tsx
+++ src/components/ui/ScrollIndicator.tsx
@@ -2,7 +2,7 @@
 // Animated vertical line at the bottom of the Hero pointing down
 export default function ScrollIndicator() {
   return (
-    <div className="absolute bottom-[90px] left-1/2 -translate-x-1/2 flex flex-col items-center gap-2">
+    <div className="absolute bottom-[120px] left-1/2 -translate-x-1/2 flex flex-col items-center gap-2">
       <span className="font-sans text-xs uppercase tracking-widest text-terracotta/60">Scroll</span>
       <div className="w-px h-15 bg-terracotta/50 animate-pulse" />
     </div>
